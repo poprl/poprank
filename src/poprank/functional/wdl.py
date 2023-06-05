2aba0a37bb1724240d042a59c19c7214abd1a8be
Aurélien Bück-Kaeffer <aurelien.buck.kaeffer@gmail.com>
2023-06-05 16:32:23 -0400

    [WIP] WDL

--- src/poprank/functional/wdl.py
+++ src/poprank/functional/wdl.py
@@ -2,6 +2,12 @@ from popcore import Interaction
 from poprank import Rate
 
 
+def typecheck(item, expected_type):
+    if not issubclass(type(item), expected_type):
+        raise TypeError(f"Expected {expected_type} but got\
+            {type(item)}")
+
+
 def windrawlose(
     players: "list[str]",
     interactions: "list[Interaction]", ratings: "list[float]",
@@ -23,7 +29,47 @@ def windrawlose(
     Returns:
         list[float]: _description_
     """
-    raise NotImplementedError()
+
+    # Typechecking
+    for player in players:
+        typecheck(player, str)
+
+    for interaction in interactions:
+        typecheck(interaction, Interaction)
+
+    for rating in ratings:
+        typecheck(rating, (float, int))
+
+    typecheck(win_value, (float, int))
+    typecheck(draw_value, (float, int))
+    typecheck(loss_value, (float, int))
+
+    ranks = [Rate(rating, 0) for rating in ratings]
+
+    for interaction in interactions:
+        best_score: int = max(interaction.outcomes)
+        indices: "list[int]" = [i for
+                                i, score in enumerate(interaction.outcomes)
+                                if score == best_score]
+
+        if len(indices) > 1:
+            for i in indices:
+                player_index = players.index(interaction.players[i])
+                new_mu = ranks[player_index].mu + draw_value
+                ranks[player_index] = Rate(new_mu, 0)
+
+        else:
+            player_index = players.index(interaction.players[indices[0]])
+            new_mu = ranks[player_index].mu + win_value
+            ranks[player_index] = Rate(new_mu, 0)
+
+        for i, player in enumerate(interaction.players):
+            if interaction.outcomes[i] < best_score:
+                player_index = players.index(player)
+                new_mu = ranks[player_index].mu + loss_value
+                ranks[player_index] = Rate(new_mu, 0)
+
+    return ranks
 
 
 def winlose(
--- test/test_wdl.py
+++ test/test_wdl.py
@@ -5,107 +5,72 @@ from poprank import Rate
 import json
 
 
+def fixtures_test(league):
+
+    # Load test data
+    with open(f"poprank/test/fixtures/2019/{league}.1.clubs.json", 'r') as f:
+
+        # Get a list of all club names
+
+        clubs: "dict[str, str | dict[str, str]]" = json.load(f)
+        names: "list[str]" = []
+        for team in clubs["clubs"]:
+            names.append(team["name"])
+
+    with open(f"poprank/test/fixtures/2019/{league}.1.json", 'r') as f:
+
+        # Get the list of all interactions between clubs
+
+        matches: "dict[str, str | dict[str, str | dict]]" = json.load(f)
+        interactions: "list[Interaction]" = []
+
+        for match in matches["matches"]:
+            players: "list[str]" = [match["team1"], match["team2"]]
+            outcomes: "list[int]" = match["score"]["ft"]
+            interactions.append(Interaction(players, outcomes))
+
+    with open(f"poprank/test/fixtures/2019/{league}.1.final.json", 'r') as f:
+
+        # Get the final ranking of all clubs
+
+        final: "list[dict[str, str | Rate]]" = []
+        for team in json.load(f)["clubs"]:
+            final.append({"id": team["id"],
+                          "rating": Rate(team["rating"], 0)})
+
+    # Assume the initial rating to be 0 for everyone
+    ratings: "list[float]" = [0.0 for team in names]
+
+    # Test that the data expected and the data calculated calculated match
+    ratings = windrawlose(players=names,
+                          interactions=interactions,
+                          ratings=ratings,
+                          win_value=3,
+                          draw_value=1,
+                          loss_value=0)
+
+    return final, ratings, names
+
+
 class TestWDLFunctional(unittest.TestCase):
 
     def test_windrawlose1(self) -> None:
-
-        # Load test data
-        with open("poprank/test/fixtures/2019/en.1.clubs.json", 'r') as f:
-
-            # Get a list of all club names
-
-            en_clubs: "dict[str, str | dict[str, str]]" = json.load(f)
-            en_names: "list[str]" = []
-            for team in en_clubs["clubs"]:
-                en_names.append(team["name"])
-
-        with open("poprank/test/fixtures/2019/en.1.json", 'r') as f:
-
-            # Get the list of all interactions between clubs
-
-            en_matches: "dict[str, str | dict[str, str | dict]]" = json.load(f)
-            en_interactions: "list[Interaction]" = []
-
-            for match in en_matches["matches"]:
-                players: "list[str]" = [match["team1"], match["team2"]]
-                outcomes: "list[int]" = match["score"]["ft"]
-                en_interactions.append(Interaction(players, outcomes))
-
-        with open("poprank/test/fixtures/2019/en.1.final.json", 'r') as f:
-
-            # Get the final ranking of all clubs
-
-            en_final: "list[dict[str, str | Rate]]" = []
-            for team in json.load(f)["clubs"]:
-                en_final.append({"id": team["id"],
-                                 "rating": Rate(team["rating"], 0)})
-
-        # Assume the initial rating to be 0 for everyone
-        en_ratings: "list[float]" = [0.0 for team in en_names]
-
-        # Test that the data expected and the data calculated calculated match
-        en_ratings = windrawlose(players=en_names,
-                                 interactions=en_interactions,
-                                 ratings=en_ratings,
-                                 win_value=3,
-                                 draw_value=1,
-                                 loss_value=0)
-
-        for team in en_final["clubs"]:
+        final, ratings, names = fixtures_test("en")
+        
+        for team in final:
             self.assertTrue(team["rating"].mu ==
-                            en_ratings[en_clubs.index(team["id"])].mu)
+                            ratings[names.index(team["id"])].mu)
             self.assertTrue(team["rating"].std ==
-                            en_ratings[en_clubs.index(team["id"])].std)
+                            ratings[names.index(team["id"])].std)
 
     def test_windrawlose2(self) -> None:
-
-        # Load test data
-        with open("poprank/test/fixtures/2019/es.1.clubs.json", 'r') as f:
-
-            # Get a list of all club names
-
-            es_clubs: "dict[str, str | dict[str, str]]" = json.load(f)
-            es_names: "list[str]" = []
-            for team in es_clubs["clubs"]:
-                es_names.append(team["name"])
-
-        with open("poprank/test/fixtures/2019/es.1.json", 'r') as f:
-
-            # Get the list of all interactions between clubs
-
-            es_matches: "dict[str, str | dict[str, str | dict]]" = json.load(f)
-            es_interactions: "list[Interaction]" = []
-
-            for match in es_matches["matches"]:
-                players: "list[str]" = [match["team1"], match["team2"]]
-                outcomes: "list[int]" = match["score"]["ft"]
-                es_interactions.append(Interaction(players, outcomes))
-
-        with open("poprank/test/fixtures/2019/es.1.final.json", 'r') as f:
-
-            # Get the final ranking of all clubs
-
-            es_final: "list[dict[str, str | Rate]]" = []
-            for team in json.load(f)["clubs"]:
-                es_final.append({"id": team["id"],
-                                 "rating": Rate(team["rating"], 0)})
-
-        # Assume the initial rating to be 0 for everyone
-        es_ratings: "list[float]" = [0.0 for team in es_names]
-
-        # Test that the data expected and calculated match
-        es_ratings = windrawlose(players=es_names,
-                                 interactions=es_interactions,
-                                 ratings=es_ratings,
-                                 win_value=3,
-                                 draw_value=1,
-                                 loss_value=0)
-
-        for team in es_final["clubs"]:
+        final, ratings, names = fixtures_test("es")
+        
+        for team in final:
             self.assertTrue(team["rating"].mu ==
-                            es_ratings[es_clubs.index(team["id"])].mu)
+                            ratings[names.index(team["id"])].mu)
             self.assertTrue(team["rating"].std ==
-                            es_ratings[es_clubs.index(team["id"])].std)
+                            ratings[names.index(team["id"])].std)
 
     def test_windrawlose3(self) -> None:
         players = [1, 2, 3]  # Players of the wrong type
