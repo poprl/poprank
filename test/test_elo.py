f9b73c962696b03eca0bbe534500777767754676
Aurélien Bück-Kaeffer <aurelien.buck.kaeffer@gmail.com>
2023-06-06 15:37:26 -0400

    [WIP] feature-elo

---
+++ test/test_elo.py
@@ -0,0 +1,9 @@
+import unittest
+from poprank.functional.elo import elo, bayeselo
+from popcore import Interaction
+from poprank import Rate
+
+
+class TestEloFunctional(unittest.TestCase):
+    def test_elo(self):
+        pass
