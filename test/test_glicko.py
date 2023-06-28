1019d5bc068a79407cbf9f26954434d79f3ea356
Scezaquer <aurelien.buck.kaeffer@gmail.com>
2023-06-28 14:34:50 -0400

    Revert "Merge branch 'main' into feature-trueskill"
    This reverts commit ffc2bf2ae79dd28ec66e3d6cc16de94e3d449583, reversing
    changes made to 0225b37300fdd3a72d417cf8d9568727b1968176.

--- test/test_glicko.py
+++ test/test_glicko.py
@@ -1,6 +1,6 @@
 import unittest
 from popcore import Interaction
-from poprank import GlickoRate, Glicko2Rate
+from poprank import Glicko1Rate, Glicko2Rate
 from poprank.functional.glicko import glicko, glicko2
 
 
@@ -9,84 +9,63 @@ class TestGlickoFunctional(unittest.TestCase):
         """Default single interaction win case"""
         players = ["a", "b"]
         interactions = [Interaction(["a", "b"], [1, 0])]
-        ratings = [
-            GlickoRate(1000, 350), GlickoRate(1000, 350)
-        ]
-        expected_results = [
-            GlickoRate(1162.212, 290.231),
-            GlickoRate(837.788, 290.231)
-        ]
+        ratings = [Glicko1Rate(1000, 350), Glicko1Rate(1000, 350)]
+        expected_results = [Glicko1Rate(1162.212, 290.231),
+                            Glicko1Rate(837.788, 290.231)]
         g_results = glicko(players, interactions, ratings)
 
         self.assertListEqual(
             # Rounding for floating point tolerance
-            [GlickoRate(round(x.mu, 3), round(x.std, 3)) for x in g_results],
-            expected_results
-        )
+            [Glicko1Rate(round(x.mu, 3), round(x.std, 3)) for x in g_results],
+            expected_results)
 
     def test_glicko_lose(self) -> None:
         """Default single interaction lose case"""
         players = ("a", "b")
         interactions = [Interaction(["a", "b"], [0, 1])]
-        ratings = [
-            GlickoRate(1000, 350), GlickoRate(1000, 350)
-        ]
-        expected_results = [
-            GlickoRate(837.788, 290.231), GlickoRate(1162.212, 290.231)
-        ]
+        ratings = [Glicko1Rate(1000, 350), Glicko1Rate(1000, 350)]
+        expected_results = [Glicko1Rate(837.788, 290.231),
+                            Glicko1Rate(1162.212, 290.231)]
         g_results = glicko(players, interactions, ratings)
 
         self.assertListEqual(
             # Rounding for floating point tolerance
-            [GlickoRate(round(x.mu, 3), round(x.std, 3)) for x in g_results],
+            [Glicko1Rate(round(x.mu, 3), round(x.std, 3)) for x in g_results],
             expected_results)
 
     def test_glicko_draw(self) -> None:
         """Default single interaction draw case"""
         players = ["a", "b"]
         interactions = [Interaction(["a", "b"], [.5, .5])]
-        ratings = [
-            GlickoRate(1000, 350), GlickoRate(1000, 350)
-        ]
-        expected_results = [
-            GlickoRate(1000.0, 290.231), GlickoRate(1000.0, 290.231)
-        ]
+        ratings = [Glicko1Rate(1000, 350), Glicko1Rate(1000, 350)]
+        expected_results = [Glicko1Rate(1000.0, 290.231),
+                            Glicko1Rate(1000.0, 290.231)]
         g_results = glicko(players, interactions, ratings)
 
         self.assertListEqual(
             # Rounding for floating point tolerance
-
-            [GlickoRate(round(x.mu, 3), round(x.std, 3)) for x in g_results],
-            expected_results
-        )
+            [Glicko1Rate(round(x.mu, 3), round(x.std, 3)) for x in g_results],
+            expected_results)
 
     def test_glicko_multiple_interactions(self):
-        """Example from Glickman's paper
-        http://www.glicko.net/glicko/glicko.pdf"""
         players = ["a", "b", "c", "d"]
-        interactions = [
-            Interaction(["a", "b"], [1, 0]),
-            Interaction(["a", "c"], [0, 1]),
-            Interaction(["a", "d"], [0, 1]),
-            Interaction(["b", "c"], [0, 1]),
-            Interaction(["b", "d"], [0, 1]),
-            Interaction(["c", "d"], [.5, .5])
-        ]
-        ratings = [
-            GlickoRate(1500, 200), GlickoRate(1400, 30),
-            GlickoRate(1550, 100), GlickoRate(1700, 300)
-        ]
+        interactions = [Interaction(["a", "b"], [1, 0]),
+                        Interaction(["a", "c"], [0, 1]),
+                        Interaction(["a", "d"], [0, 1]),
+                        Interaction(["b", "c"], [0, 1]),
+                        Interaction(["b", "d"], [0, 1]),
+                        Interaction(["c", "d"], [.5, .5])]
+        ratings = [Glicko1Rate(1500, 200), Glicko1Rate(1400, 30),
+                   Glicko1Rate(1550, 100), Glicko1Rate(1700, 300)]
 
         g_results = glicko(players, interactions, ratings)
-        g_results = [
-            GlickoRate(round(x.mu, 3), round(x.std, 3))
-            for x in g_results
-        ]
+        g_results = [Glicko1Rate(round(x.mu, 3), round(x.std, 3))
+                     for x in g_results]
 
-        expected_results = [
-            GlickoRate(1464.106, 151.399), GlickoRate(1396.046, 29.800),
-            GlickoRate(1588.344, 92.598), GlickoRate(1742.969, 194.514)
-        ]
+        expected_results = [Glicko1Rate(1464.106, 151.399),
+                            Glicko1Rate(1396.046, 29.800),
+                            Glicko1Rate(1588.344, 92.598),
+                            Glicko1Rate(1742.969, 194.514)]
 
         self.assertListEqual(g_results, expected_results)
 
@@ -94,28 +73,23 @@ class TestGlickoFunctional(unittest.TestCase):
         """No interaction case"""
         players = ["a", "b"]
         interactions = []
-        ratings = [
-            GlickoRate(1000, 350), GlickoRate(1000, 350)
-        ]
-        expected_results = [
-            GlickoRate(1000.0, 350), GlickoRate(1000.0, 350)
-        ]
-
+        ratings = [Glicko1Rate(1000, 350), Glicko1Rate(1000, 350)]
+        expected_results = [Glicko1Rate(1000.0, 350),
+                            Glicko1Rate(1000.0, 350)]
         g_results = glicko(players, interactions, ratings)
 
         self.assertListEqual(
             # Rounding for floating point tolerance
-            [GlickoRate(round(x.mu, 3), round(x.std, 3)) for x in g_results],
+            [Glicko1Rate(round(x.mu, 3), round(x.std, 3)) for x in g_results],
             expected_results)
 
 
 class TestGlicko2Functional(unittest.TestCase):
     def round_glicko2(self, g_results) -> "list[Glicko2Rate]":
         g_volatilities = [x.volatility for x in g_results]
-        g_results = [
-            Glicko2Rate(round(x.mu, 3), round(x.std, 3))
-            for x in g_results
-        ]
+        g_results = [Glicko2Rate(round(x.mu, 3), round(x.std, 3))
+                     for x in g_results]
+
         for x, _ in enumerate(g_results):
             g_results[x].volatility = round(g_volatilities[x], 10)
 
@@ -124,18 +98,12 @@ class TestGlicko2Functional(unittest.TestCase):
     def test_glicko2_win(self) -> None:
         """Default single interaction win case"""
         players = ["a", "b"]
-        interactions = [
-            Interaction(["a", "b"], [1, 0])
-        ]
-        ratings = [
-            Glicko2Rate(1000, 350), Glicko2Rate(1000, 350)
-        ]
-        expected_results = [
-            Glicko2Rate(1162.311, 290.319), Glicko2Rate(837.689, 290.319)
-        ]
-        expected_volatilities = [
-            0.060000000000000005, 0.060000000000000005
-        ]
+        interactions = [Interaction(["a", "b"], [1, 0])]
+        ratings = [Glicko2Rate(1000, 350), Glicko2Rate(1000, 350)]
+        expected_results = [Glicko2Rate(1162.311, 290.319),
+                            Glicko2Rate(837.689, 290.319)]
+        expected_volatilities = [0.060000000000000005,
+                                 0.060000000000000005]
 
         for x, _ in enumerate(expected_results):
             expected_results[x].volatility = \
@@ -150,15 +118,11 @@ class TestGlicko2Functional(unittest.TestCase):
         """Default single interaction lose case"""
         players = ("a", "b")
         interactions = [Interaction(["a", "b"], [0, 1])]
-        ratings = [
-            Glicko2Rate(1000, 350), Glicko2Rate(1000, 350)
-        ]
-        expected_results = [
-            Glicko2Rate(837.689, 290.319), Glicko2Rate(1162.311, 290.319)
-        ]
-        expected_volatilities = [
-            0.060000000000000005, 0.060000000000000005
-        ]
+        ratings = [Glicko2Rate(1000, 350), Glicko2Rate(1000, 350)]
+        expected_results = [Glicko2Rate(837.689, 290.319),
+                            Glicko2Rate(1162.311, 290.319)]
+        expected_volatilities = [0.060000000000000005,
+                                 0.060000000000000005]
 
         for x, _ in enumerate(expected_results):
             expected_results[x].volatility = \
@@ -172,18 +136,12 @@ class TestGlicko2Functional(unittest.TestCase):
     def test_glicko2_draw(self) -> None:
         """Default single interaction draw case"""
         players = ["a", "b"]
-        interactions = [
-            Interaction(["a", "b"], [.5, .5])
-        ]
-        ratings = [
-            Glicko2Rate(1000, 350), Glicko2Rate(1000, 350)
-        ]
-        expected_results = [
-            Glicko2Rate(1000.0, 290.319), Glicko2Rate(1000.0, 290.319)
-        ]
-        expected_volatilities = [
-            0.060000000000000005, 0.060000000000000005
-        ]
+        interactions = [Interaction(["a", "b"], [.5, .5])]
+        ratings = [Glicko2Rate(1000, 350), Glicko2Rate(1000, 350)]
+        expected_results = [Glicko2Rate(1000.0, 290.319),
+                            Glicko2Rate(1000.0, 290.319)]
+        expected_volatilities = [0.060000000000000005,
+                                 0.060000000000000005]
 
         for x, _ in enumerate(expected_results):
             expected_results[x].volatility = \
@@ -195,34 +153,29 @@ class TestGlicko2Functional(unittest.TestCase):
         self.assertListEqual(g_results, expected_results)
 
     def test_glicko2_multiple_interactions(self):
-        """Example from Glickman's paper
-        http://www.glicko.net/glicko/glicko2.pdf"""
         players = ["a", "b", "c", "d"]
-        interactions = [
-            Interaction(["a", "b"], [1, 0]),
-            Interaction(["a", "c"], [0, 1]),
-            Interaction(["a", "d"], [0, 1]),
-            Interaction(["b", "c"], [0, 1]),
-            Interaction(["b", "d"], [0, 1]),
-            Interaction(["c", "d"], [.5, .5])
-        ]
-        ratings = [
-            Glicko2Rate(1500, 200), Glicko2Rate(1400, 30),
-            Glicko2Rate(1550, 100), Glicko2Rate(1700, 300)]
+        interactions = [Interaction(["a", "b"], [1, 0]),
+                        Interaction(["a", "c"], [0, 1]),
+                        Interaction(["a", "d"], [0, 1]),
+                        Interaction(["b", "c"], [0, 1]),
+                        Interaction(["b", "d"], [0, 1]),
+                        Interaction(["c", "d"], [.5, .5])]
+        ratings = [Glicko2Rate(1500, 200), Glicko2Rate(1400, 30),
+                   Glicko2Rate(1550, 100), Glicko2Rate(1700, 300)]
         tau = 0.5
 
         g_results = glicko2(players, interactions, ratings, tau)
         g_results = self.round_glicko2(g_results)
 
-        expected_results = [
-            Glicko2Rate(1464.051, 151.517), Glicko2Rate(1395.575, 31.522),
-            Glicko2Rate(1588.701, 93.027), Glicko2Rate(1742.991, 194.563)
-        ]
+        expected_results = [Glicko2Rate(1464.051, 151.517),
+                            Glicko2Rate(1395.575, 31.522),
+                            Glicko2Rate(1588.701, 93.027),
+                            Glicko2Rate(1742.991, 194.563)]
 
-        expected_volatilities = [
-            0.0599959842864885, 0.06000183590775173,
-            0.060000172066474, 0.060000000000000005
-        ]
+        expected_volatilities = [0.0599959842864885,
+                                 0.06000183590775173,
+                                 0.060000172066474,
+                                 0.060000000000000005]
 
         for x, _ in enumerate(expected_results):
             expected_results[x].volatility = \
@@ -234,15 +187,10 @@ class TestGlicko2Functional(unittest.TestCase):
         """No interactions case"""
         players = ["a", "b"]
         interactions = []
-        ratings = [
-            Glicko2Rate(1000, 350), Glicko2Rate(1000, 350)
-        ]
-        expected_results = [
-            Glicko2Rate(1000.0, 350.155), Glicko2Rate(1000.0, 350.155)
-        ]
-        expected_volatilities = [
-            0.06, 0.06
-        ]
+        ratings = [Glicko2Rate(1000, 350), Glicko2Rate(1000, 350)]
+        expected_results = [Glicko2Rate(1000.0, 350.155),
+                            Glicko2Rate(1000.0, 350.155)]
+        expected_volatilities = [0.06, 0.06]
 
         for x, _ in enumerate(expected_results):
             expected_results[x].volatility = \
