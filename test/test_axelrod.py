3dcfece4034345b1d74cb1d9611bdb126a6a6282
Scezaquer <aurelien.buck.kaeffer@gmail.com>
2023-10-11 13:25:57 -0400

    edited axelrod test

--- test/test_axelrod.py
+++ test/test_axelrod.py
@@ -14,9 +14,29 @@ def score_to_chess(outcome):
     return (0.5, 0.5)
 
 
+def payoff_to_interac(players, payoff):
+    interactions = []
+
+    for i, p1 in enumerate(players):
+        for j, p2 in enumerate(players[i+1:]):
+
+            outcome = (payoff[i][j], payoff[j][i])
+            outcome = score_to_chess(outcome)
+
+            interac = Interaction(
+                players=[str(p1) + str(i), str(p2) + str(j+i+1)],
+                outcomes=outcome)
+
+            interactions.append(interac)
+
+    return interactions
+
+
 class TestAxelrodImplementation(unittest.TestCase):
 
     def test_axelrod_bayeselo(self):
+
+        # Create initial population
 
         players = [axl.Cooperator(),
                    axl.Defector(),
@@ -29,54 +49,47 @@ class TestAxelrodImplementation(unittest.TestCase):
                    axl.ArrogantQLearner(),
                    axl.Bully()]
 
+        # Add the initial agents to the population
+
         pop = Population()
-
-        branches = [pop.branch(str(p), auto_rename=True) for p in players]
-
-        for p, b in zip(players, branches):
+        branches = []
+        for p in players:
+            b = pop.branch(str(p), auto_rename=True)
             pop.checkout(b)
             pop.commit(p)
+            branches.append(b)
 
         for gen in range(7):
-            interactions = []
-            elos = [EloRate(mu=0., std=0.) for _ in players]
+
+            # Rank current agents
+
+            elos = [EloRate() for _ in players]
 
             tournament = axl.Tournament(players)
             results = tournament.play(progress_bar=False)
 
-            for i, p1 in enumerate(players):
-                for j, p2 in enumerate(players[i+1:]):
-                    outcome = (results.payoff_matrix[i][j],
-                               results.payoff_matrix[j][i])
-                    interactions.append(
-                        Interaction(players=[str(p1) + str(i),
-                                             str(p2) + str(j+i+1)],
-                                    outcomes=score_to_chess(outcome)))
+            interactions = payoff_to_interac(players, results.payoff_matrix)
 
             player_names = [str(p) + str(i) for i, p in enumerate(players)]
             ratings = bayeselo(player_names, interactions, elos)
 
-            ranked_players = sorted(
-                zip(ratings, players),
+            ratings, players, branches = [list(t) for t in zip(*sorted(
+                zip(ratings, players, branches),
                 key=lambda x: x[0].mu,
                 reverse=True
-            )
-            ranked_players = [player for elo, player in ranked_players]
-            ratings.sort(key=lambda x: x.mu, reverse=True)
-            ratings = [EloRate(round(r.mu), 0) for r in ratings]
+            ))]
 
             print(f"Round {gen}")
-            for x, y in zip(ranked_players, ratings):
-                print(f"{str(x):>15}: {y.mu}")
+            for x, y in zip(ratings, players):
+                print(f"{str(y):>15}: {x.mu}")
             print()
 
-            first = results.ranking[0]
-            last = results.ranking[-1]
+            # Remove the worst player and replace with a copy of the best
 
-            pop.checkout(branches[first])
-            branches[last] = pop.branch(str(players[first]), auto_rename=True)
+            pop.checkout(branches[0])
+            branches[-1] = pop.branch(str(players[0]), auto_rename=True)
 
-            players[last] = players[first]
+            players[-1] = players[0]
 
             for p, b in zip(players, branches):
                 pop.checkout(b)
